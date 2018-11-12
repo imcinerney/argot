2e0be84097d90462bd5f3fd9b9d5eadadf85149b
Ian McInerney <ian.g.mcinerney@gmail.com>
2018-11-12 17:13:05 -0500

    Added basic django views for dictionary

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from . import models
 
-# Create your views here.
+
+def index(request):
+    latest_words_added = models.BaseWord.objects.order_by('-id')[:5]
+    context = {
+        'latest_words_added': latest_words_added,
+    }
+    return render(request, 'dictionary/index.html', context)
+
+
+def detail(request, base_word_id):
+    word = get_object_or_404(models.BaseWord, pk=base_word_id)
+    return render(request, 'dictionary/detail.html', {'word': word})
+
+
+def results(request, base_word_id):
+    response = 'You are looking at the name of base_word %s'
+    return HttpResponse(response % base_word_id)
+
+
+def vote(request, base_word_id):
+    return HttpResponse(f'You are voting on: {base_word_id}')
