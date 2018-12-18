1256ba637e4a619faf8438ac67b569e9a5889f1d
Ian McInerney <ian.g.mcinerney@gmail.com>
2018-12-18 17:46:42 -0500

    Added migration to load data initially

---
+++ dictionary/migrations/0016_load_static_data.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+from django.core.management import call_command
+from dictionary import models
+import dictionary.sql_views as sql_views
+import os
+
+
+class Migration(migrations.Migration):
+
+
+    def load_data(apps, schema_editor):
+        fixture_file = os.path.join((os.path.dirname(__file__)), '../fixtures',
+                                    'initial_data.json')
+        call_command('loaddata', fixture_file)
+
+    dependencies = [
+        ('dictionary', '0015_synonymstolookup'),
+    ]
+
+    operations = [
+        migrations.RunPython(load_data)
+    ]
